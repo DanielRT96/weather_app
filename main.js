fc2ea0a35c8de989588d1b4fcece16b774c9bcbd
DanielRT96 <reti.daniel96@gmail.com>
2020-09-24 13:56:33 +0200

    Fixes, loading data, update UI

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
-// const weatherApi = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=Budapest&appid=2e0f37fe8a0d411129e27f36b2a7a02f";
+// const weatherApi ="https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=Budapest&appid=2e0f37fe8a0d411129e27f36b2a7a02f";
 
 //DOMs strings
 const cityName = document.querySelector(".city__name");
 const lowestTemp = document.querySelector(".lowest__temperature");
-const highestTemp = doucument.querySelector(".highest__temperature");
-const todayName = doucument.querySelector(".today__name");
-const todayDate = doucument.querySelector(".today__date");
-const todayWind = doucument.querySelector(".today__wind");
-const rain = doucument.querySelector(".rain");
-const currentStatusStr = doucument.querySelector(".current__status__string");
-const currentCelsius = doucument.querySelector(".current__celsius");
-const day = doucument.querySelector(".day");
-
-
-
-
-
+const highestTemp = document.querySelector(".highest__temperature");
+const todayName = document.querySelector(".today__name");
+const todayDate = document.querySelector(".today__date");
+const todayWind = document.querySelector(".today__wind");
+const rain = document.querySelector(".rain");
+const currentStatusStr = document.querySelector(".current__status__string");
+const currentCelsius = document.querySelector(".current__celsius");
+const day = document.querySelector(".day");
+const celsius = document.querySelector(".celsius");
 
 // // API Call
-async function callAPI(apiURL) {
-  try {
-    const result = await fetch(apiURL);
-    const data = await result.json();
-    console.log(data);
-    cityName.innerHTML = data.city.name;
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function callAPI(apiURL) {
+//   try {
+//     const result = await fetch(apiURL);
+//     const data = await result.json();
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-callAPI(weatherApi);
+const weatherData = JSON.parse("data");
+console.log(weatherData);
+// callAPI(weatherApi);

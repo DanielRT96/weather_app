f50a2d96f9aa56b8136d31d89c759a7c4c6c467e
DanielRT96 <reti.daniel96@gmail.com>
2020-09-28 11:00:25 +0200

    Filling up the UI with API data

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// const weatherApi ="https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=Budapest&appid=2e0f37fe8a0d411129e27f36b2a7a02f";
+const weatherApi =
+  "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=Budapest&appid=2e0f37fe8a0d411129e27f36b2a7a02f";
 
 //DOMs strings
 const cityName = document.querySelector(".city__name");
@@ -7,24 +8,71 @@ const highestTemp = document.querySelector(".highest__temperature");
 const todayName = document.querySelector(".today__name");
 const todayDate = document.querySelector(".today__date");
 const todayWind = document.querySelector(".today__wind");
-const rain = document.querySelector(".rain");
+const rainChance = document.querySelector(".rain");
 const currentStatusStr = document.querySelector(".current__status__string");
 const currentCelsius = document.querySelector(".current__celsius");
 const day = document.querySelector(".day");
 const celsius = document.querySelector(".celsius");
 
-// // API Call
-// async function callAPI(apiURL) {
-//   try {
-//     const result = await fetch(apiURL);
-//     const data = await result.json();
-//     console.log(data);
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+// // // API Call
+async function callAPI(apiURL) {
+  try {
+    const result = await fetch(apiURL);
+    const data = await result.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-const weatherData = JSON.parse("data");
-console.log(weatherData);
-// callAPI(weatherApi);
+//Budapest
+const baseWeather = async () => {
+  const data = await callAPI(weatherApi);
+
+  cityName.innerHTML = data.city.name;
+
+  let date_string = data.list[0].dt_txt;
+  let dateApi = new Date(date_string);
+  todayName.innerHTML = getTodayString(dateApi.getDay());
+  todayDate.innerHTML = dateApi.toLocaleDateString("en-US");
+
+  let windSpeed = Math.round(data.list[0].wind.speed);
+  todayWind.innerHTML = `Wind ${windSpeed}km/h`;
+
+  //let chanceRain = data.list[0].rain.3h * 100;
+  //lowestTemp.innerHTML = celsiusCalc(data.list[0].main.temp_min);
+  //highestTemp.innerHTML = celsiusCalc(data.list[0].main.temp_max);
+  console.log(data);
+};
+
+const getTodayString = (todayNumber) => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let todayString = days[todayNumber];
+  return todayString;
+};
+
+const celsiusCalc = (fahrenheit) => {
+  let fTemp = fahrenheit;
+  let fToC = Math.round((fTemp - 32) / 1.8);
+  console.log(fToC);
+  return fToC;
+};
+
+baseWeather();
+
+
+const updateUi = () => {
+
+}
+
+let d = new Date();
+console.log(d);
